3a6fe6a0e4d12c269548bb8daf6d18be12d83712
shruti <shruti@riverus.in>
2020-07-21 15:00:03 +0530

    ts config fix

--- src/db/connection.js
+++ src/db/connection.js
@@ -36,7 +36,6 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
     }
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.Connection = exports.severusClient = exports.stageClient = void 0;
 var pg_1 = require("pg");
 var stageParams = {
     user: 'deadpool',
